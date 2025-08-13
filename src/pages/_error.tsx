fd3fd391741016e271239f3754ab2a36ebb6bb29
zapvida <allzapvida@gmail.com>
2025-08-13 19:05:05 -0300

    fix(error): tipar _error.tsx e remover any implícito

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,17 +1,16 @@
-import * as Sentry from "@sentry/nextjs";
-import Error from "next/error";
+// src/pages/_error.tsx
+import { NextPageContext } from 'next'
+import Error from 'next/error'
 
-const CustomErrorComponent = (props) => {
-  return <Error statusCode={props.statusCode} />;
-};
+type ErrorProps = { statusCode?: number }
 
-CustomErrorComponent.getInitialProps = async (contextData) => {
-  // In case this is running in a serverless function, await this in order to give Sentry
-  // time to send the error before the lambda exits
-  await Sentry.captureUnderscoreErrorException(contextData);
+const CustomErrorComponent = ({ statusCode = 500 }: ErrorProps) => {
+  return <Error statusCode={statusCode} />
+}
 
-  // This will contain the status code of the response
-  return Error.getInitialProps(contextData);
-};
+CustomErrorComponent.getInitialProps = ({ res, err }: NextPageContext): ErrorProps => {
+  const statusCode = res?.statusCode ?? err?.statusCode ?? 404
+  return { statusCode }
+}
 
-export default CustomErrorComponent;
+export default CustomErrorComponent
